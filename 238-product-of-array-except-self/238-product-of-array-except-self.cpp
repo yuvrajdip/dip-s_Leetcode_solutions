2b6a99d6631e5b0f3a25b18e2c4e2673b0a33cc8
Deba Dip Bhowmik <66591733+yuvrajdip@users.noreply.github.com>
2022-05-31 00:13:46 +0600

    Time: 532 ms (5.29%), Space: 28 MB (7.07%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> premult , sufmult ;
+        
+        int pre= 1 , suf=1 ; 
+        
+        int n= nums.size();
+        for( int i= 0 ; i<n ; i++){
+            pre *= nums[i];
+            
+            premult.emplace_back( pre );
+        }
+        
+        for( auto i : premult )
+            cout<<i << " ";
+        cout << endl;
+        
+        for(int i=n-1 ; i>=0 ; i-- ){
+            suf *= nums[i];
+            
+            sufmult.emplace_back( suf );
+        }
+        
+        reverse( sufmult.begin() , sufmult.end());
+        
+        for( auto i : sufmult)
+            cout<<i << " ";
+        cout<< endl;
+        
+        vector<int>store;
+        for( int i= 0 ; i<n ; i++ )
+        {
+            int res = 0 ;
+            
+            if( nums[i] !=0 )
+                res = premult[i]/nums[i] * sufmult[i]/nums[i];
+            else if( nums[i]==0 ){
+                if( i>=1 and i<= n-2 )
+                    res = premult[i-1] * sufmult[i+1];
+                else if( i==0 )
+                    res = sufmult[i+1];
+                else if( i==n-1 )
+                    res = premult[i-1];
+            }
+            
+            store.emplace_back( res );
+        }
+        
+        return store ;
+    }
+};

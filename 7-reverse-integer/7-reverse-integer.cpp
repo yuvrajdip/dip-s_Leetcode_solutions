b548a9da6247318be3e8f49a30a0eb5f3c00a58c
Deba Dip Bhowmik <dipbhowmik2018@gmail.com>
2022-08-11 23:50:08 +0600

    Time: 12 ms (5.23%), Space: 5.9 MB (76.02%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    int reverse(int n) {
+    int reverse(int x) {
         
         int ans = 0 ;
         
-        while(n != 0){ // Here n !=0  is a mandatory condition , you can't give n>0
-            int digit = n%10;
+        while(x != 0){ // n !=0  is a mandatory. you can give n>0 but it will give WA for negative integers
+            
+            int digit = x%10;
             
             if( (ans> INT_MAX/10 ) || ( ans< INT_MIN/10 ) )
             {
                 return 0;
             }
         
-            ans = ans*10 + digit ;
+            ans = (ans*10) + digit ;
             
-            n = n/10 ;
+            x = x/10 ;
         }
         
         return ans ;

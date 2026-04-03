57c8668cae4e9000466d66906b40eeb72c939e9b
Deba Dip Bhowmik <dipbhowmik2018@gmail.com>
2026-04-03 15:27:52 +0600

    Time: 10 ms (72.22%), Space: 35.1 MB (53.59%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+
+    int func( vector<int>& weights , int capacity )
+    {
+        int loadPerDay = 0 , daysNeeded = 1 ;
+        for( int j=0 ; j< weights.size() ; j++ )
+        {
+            loadPerDay+= weights[j];
+            if( loadPerDay > capacity )
+            {
+                loadPerDay = weights[j];
+                daysNeeded += 1 ;
+            }
+        }
+
+        return daysNeeded ;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        
+        int maxi = INT_MIN , n = weights.size() , sumOfAllWeights = 0 ;
+
+        for( int i=0; i<n ; i++ )
+        {
+            maxi = max( weights[i], maxi );
+            sumOfAllWeights += weights[i];
+        }
+
+        int low = maxi , high = sumOfAllWeights ;
+
+        while( low <= high )
+        {
+            int mid = low + ( high-low )/2 ;
+
+            if( func(weights, mid ) <= days )
+            {
+                high = mid-1 ;
+            }
+            else
+            {
+                low = mid + 1 ;
+            }
+        }
+
+        return low ;
+
+    }
+};
